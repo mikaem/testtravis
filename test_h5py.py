0c419bb1d8462085d9b506ab8590af8873e15161
Mikael Mortensen <mikaem@math.uio.no>
2016-03-13 13:14:18 +0100

    Add h5py test

--- test_h5py.py
+++ test_h5py.py
@@ -1,10 +1,10 @@
 import pytest
 import mpi4py
 from mpi4py.MPI import COMM_WORLD as comm
-#import h5py
+import h5py
 
 def test_mpi4py():
     assert True
     
-#def test_h5py():
-    #f = h5py.File('tmp.h5', 'w', driver='mpio', comm=comm)
+def test_h5py():
+    f = h5py.File('tmp.h5', 'w', driver='mpio', comm=comm)
