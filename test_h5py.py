952ca7c9a2aad7c269ff1f4d4411be417b48e9ce
Mikael Mortensen <mikaem@math.uio.no>
2016-03-15 18:55:12 +0100

    test dolfin

--- test_h5py.py
+++ test_h5py.py
@@ -4,8 +4,10 @@ from dolfin import *
 from mpi4py.MPI import COMM_WORLD as comm
 import h5py
 
-def test_mpi4py():
-    assert True
+def test_dolfin():
+    mesh = UnitSquareMesh(4, 4)
+    V = FunctionSpace(mesh, 'CG', 1)
+    u = interpolate(Expression("x[0]"), V)
     
 def test_h5py():
     f = h5py.File('tmp.h5', 'w', driver='mpio', comm=comm)
